2c6107d7ad9e043ea1cd269a9b3f4aeb083dbdc6
fede-vazquez <fedenv2000@gmail.com>
2025-07-31 00:19:37 -0300

    Agrega funcionalidad de seguimineto al mouse al componente de Cursor dentro del proyecto cf.

--- repaso_react/front/cf/src/Cursor.jsx
+++ repaso_react/front/cf/src/Cursor.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 
 export default function Cursor() {
     const [border, setBorder] = useState("border-blue-600");
-
     useEffect(() => {
         setTimeout(() => {
             setBorder("border-green-700");
@@ -15,12 +14,29 @@ export default function Cursor() {
 
     // Este useEffect va a ser para cuando se empiece a mover.
     // Tarea (windows.client // te da las coordenadas del cursor // hacerlo con style y no con className)
-    const [pos, setPos] = useState({ x: 0, y: 0 });
-    useEffect(() => {}, []);
+
+    useEffect(() => {
+        // opción sin usar useState
+        const handleMouseMove = e => {
+            iconEl.style.transform = `translate(${
+                e.clientX - iconEl.offsetWidth / 2
+            }px, ${e.clientY - iconEl.offsetHeight / 2}px)`;
+            // iconEl.style.top = `${e.clientY - iconEl.offsetHeight / 2}px`;
+            // iconEl.style.left = `${e.clientX - iconEl.offsetWidth / 2}px`;
+        };
+
+        const iconEl = document.getElementById("icon");
+        window.onmousemove = handleMouseMove;
+
+        return () => {
+            window.onmousemove = null;
+        };
+    }, []);
 
     return (
         <div
-            className={`${border} z-[-1] top-0 left-0 bg-gray-900 absolute w-20 h-20 border-2 rounded-full transition duration-200`}
+            id="icon"
+            className={`${border} z-[-1] top-0 left-0 bg-gray-900 absolute w-20 h-20 border-2 rounded-full`}
         ></div>
     );
 }
