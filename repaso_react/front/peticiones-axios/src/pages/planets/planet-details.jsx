ae5a7cc3940443169d0ab813202e0a60ef3783f5
fede-vazquez <fedenv2000@gmail.com>
2025-08-08 12:55:57 -0300

    Agrega detalles de personajes en la página de detalles del planeta

--- repaso_react/front/peticiones-axios/src/pages/planets/planet-details.jsx
+++ repaso_react/front/peticiones-axios/src/pages/planets/planet-details.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "wouter";
+import { Link, useParams } from "wouter";
 import { getOnePlanetById } from "@/services/dragon-ball-api";
 import { useEffect, useState } from "react";
 
@@ -30,6 +30,40 @@ export default function PlanetDetail() {
                         <p className="py-6">{planet.description}</p>
                     </div>
                 </div>
+            </section>
+
+            <br />
+
+            <section className="w-3/4 mx-auto flex flex-col gap-10">
+                <h2 className="text-3xl">Personajes del planeta: </h2>
+                {planet?.characters?.length > 0 ? (
+                    planet?.characters?.map(char => (
+                        <article className="hero bg-base-200">
+                            <div className="hero-content flex-col lg:flex-row-reverse">
+                                <img
+                                    src={char.image}
+                                    className="max-w-sm rounded-lg shadow-2xl max-h-52"
+                                />
+                                <div>
+                                    <h3 className="text-xl font-bold">
+                                        {char.name}
+                                    </h3>
+                                    <p className="py-6 max-h-60 overflow-y-scroll">
+                                        {char.description}
+                                    </p>
+                                    <Link
+                                        href={`/characters/${char.id}`}
+                                        className="btn btn-primary"
+                                    >
+                                        More info
+                                    </Link>
+                                </div>
+                            </div>
+                        </article>
+                    ))
+                ) : (
+                    <p>No se encontraron personajes en este planeta</p>
+                )}
             </section>
         </main>
     );
