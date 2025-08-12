d16a92467996d2b23252b84a23d8a8eba3168639
fede-vazquez <fedenv2000@gmail.com>
2025-08-12 20:46:56 -0300

    Refactoriza función en cinchada para que sea más simple cambiar los colores de los equipos

--- repaso_react/front/states-events/src/components/cinchada-project/cinchada.jsx
+++ repaso_react/front/states-events/src/components/cinchada-project/cinchada.jsx
@@ -1,41 +1,58 @@
 import { useState } from "react";
 import Equipo from "./equipo";
 
+const equipoA = "red";
+const equipoB = "green";
 export default function Cinchada() {
-    const [cuerdaEquipos, setCuerdaEquipos] = useState({ red: 5, green: 5 });
+    const [cuerdaEquipos, setCuerdaEquipos] = useState({
+        [equipoA]: 5,
+        [equipoB]: 5,
+    });
 
-    const handleTirar = color => {
-        if (color === "red") {
-            setCuerdaEquipos(prev => ({
-                red: prev.red + 1,
-                green: prev.green - 1,
-            }));
-        } else {
-            setCuerdaEquipos(prev => ({
-                red: prev.red - 1,
-                green: prev.green + 1,
-            }));
-        }
+    const handleTirar = colorSeleccionado => {
+        const colores = Object.keys(cuerdaEquipos);
+
+        const otroColor = colores.find(color => color != colorSeleccionado);
+
+        setCuerdaEquipos(prev => ({
+            [colorSeleccionado]: prev[colorSeleccionado] + 1,
+            [otroColor]: prev[otroColor] - 1,
+        }));
     };
+
+    // const [cuerdaEquipos, setCuerdaEquipos] = useState({ red: 5, green: 5 });
+    // const handleTirar = color => {
+    //     if (color === "red") {
+    //         setCuerdaEquipos(prev => ({
+    //             red: prev.red + 1,
+    //             green: prev.green - 1,
+    //         }));
+    //     } else {
+    //         setCuerdaEquipos(prev => ({
+    //             red: prev.red - 1,
+    //             green: prev.green + 1,
+    //         }));
+    //     }
+    // };
 
     return (
         <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
             <Equipo
-                color={"red"}
-                distancia={cuerdaEquipos.red}
+                color={equipoA}
+                distancia={cuerdaEquipos[equipoA]}
                 onTirar={handleTirar}
             />
             <span>
                 --------------------------------------------------------------
-                <div>Longitud: {cuerdaEquipos.green + cuerdaEquipos.red}m</div>
+                <div>
+                    Longitud: {cuerdaEquipos[equipoB] + cuerdaEquipos[equipoA]}m
+                </div>
             </span>
             <Equipo
-                color={"green"}
-                distancia={cuerdaEquipos.green}
+                color={equipoB}
+                distancia={cuerdaEquipos[equipoB]}
                 onTirar={handleTirar}
             />
         </div>
     );
 }
-
-// Simplificar el código de la función tirar
