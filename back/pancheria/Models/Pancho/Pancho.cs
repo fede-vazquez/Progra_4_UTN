2eee54abec29b86240c189a209f0bce7d9721475
fede-vazquez <fedenv2000@gmail.com>
2025-09-18 16:05:34 -0300

    Hace publico el atributo de Nombre en la clase Pancho.

--- back/pancheria/Models/Pancho/Pancho.cs
+++ back/pancheria/Models/Pancho/Pancho.cs
@@ -3,14 +3,9 @@
     public class Pancho
     {
         public int Id { get; set; }
-        private string Nombre = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
         public bool IsVegano { get; set; }
         public List<string> Aderezos { get; set; } = new List<string>();
         public double Precio { get; set; }
-
-        public string GetNombre() => Nombre;
-        public void SetNombre(string Nombre) {
-            this.Nombre = Nombre;
-        }
     }
 }
--- back/pancheria/Services/PanchoServices.cs
+++ back/pancheria/Services/PanchoServices.cs
@@ -12,15 +12,15 @@ namespace pancheria.Services
     public class PanchoServices : IPanchoServices
     {
         private List<Pancho> _panchos = new() {
-            new() { Id = 1, IsVegano = false, Aderezos = {"Mayonesa", "Mostaza" } },
-            new() { Id = 2, IsVegano = false, Aderezos = {"Mostaza"} },
-            new() { Id = 3, IsVegano = false, Aderezos = {} },
-            new() { Id = 4, IsVegano = true, Aderezos = {"Mayonesa"} }
+            new() { Id = 1, Nombre="Pancho 1", IsVegano = false, Aderezos = {"Mayonesa", "Mostaza" } },
+            new() { Id = 2, Nombre="Pancho 2", IsVegano = false, Aderezos = {"Mostaza"} },
+            new() { Id = 3, Nombre="Pancho 3", IsVegano = false, Aderezos = {} },
+            new() { Id = 4, Nombre="Pancho 4", IsVegano = true, Aderezos = {"Mayonesa"} }
             };
 
         public List<PanchosDTO> GetPanchos()
         {
-            return _panchos.Select(p => new PanchosDTO { Id = p.Id, Nombre = p.GetNombre(), Precio = p.Precio }).ToList();
+            return _panchos.Select(p => new PanchosDTO { Id = p.Id, Nombre = p.Nombre, Precio = p.Precio }).ToList();
         }
 
         public Pancho GetPanchoById(int id)
@@ -40,7 +40,7 @@ namespace pancheria.Services
             List<Pancho> panchosConAderezo = _panchos.FindAll(p => p.Aderezos.Contains(aderezo));
             if (panchosConAderezo.Count == 0)
             {
-                return panchosConAderezo.Select(p => new PanchoConAderezosDTO { Id = p.Id, Nombre = p.GetNombre(), Precio = p.Precio, Aderezos = p.Aderezos }).ToList();
+                return panchosConAderezo.Select(p => new PanchoConAderezosDTO { Id = p.Id, Nombre = p.Nombre, Precio = p.Precio, Aderezos = p.Aderezos }).ToList();
             }
             else
             {
