85546e5f1a6898b4600a503dd9da1268c4f1ae3a
fede-vazquez <fedenv2000@gmail.com>
2025-10-13 11:24:48 -0300

    Agrega proyecto inicial para implementar auth.

---
+++ back/Auth/Auth/Program.cs
@@ -0,0 +1,39 @@
+using Auth.Config;
+using Auth.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddAutoMapper(opts => { }, typeof(Mapping));
+
+builder.Services.AddDbContext<ApplicationDbContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("authConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
